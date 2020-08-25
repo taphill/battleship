f1f32ea48f54c92827dab07636de6f35c0adde14
Taylor Phillips <taylorscottphillips@gmail.com>
2020-08-25 12:58:46 -0500

    Add render method for Cell class

--- lib/cell.rb
+++ lib/cell.rb
@@ -9,7 +9,11 @@ class Cell
   end
 
   def empty?
-    has_ship
+    if has_ship
+      false
+    else
+      true
+    end
   end
 
   def place_ship(ship_to_add)
@@ -19,11 +23,29 @@ class Cell
 
   def fire_upon
     self.fired_at = true
+    return nil if empty?
+
     ship.length -= 1
   end
 
   def fired_upon?
     fired_at
+  end
+
+  def render(show_ship = false)
+    if fired_upon? && empty? == false && ship.sunk?
+      "X"
+    elsif fired_upon? && empty?
+      "M"
+    elsif fired_upon? && (empty? == false)
+      "H"
+    elsif (fired_upon? == false) && show_ship == true
+      "S"
+    elsif fired_upon? == false
+      "."
+    else
+      "ERROR"
+    end
   end
 
   private
