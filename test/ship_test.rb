2b7c8e942a93b4421e19aa0a2ea654e776b0a8ba
Taylor Phillips <taylorscottphillips@gmail.com>
2020-08-25 08:19:02 -0500

    Add Ship class and ShipTest

---
+++ test/ship_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+  def test_it_exists
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_instance_of Ship, cruiser
+  end
+
+  def test_it_has_attributes
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal 'Cruiser', cruiser.name
+    assert_equal 3, cruiser.length
+  end
+end
