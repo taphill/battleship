ed55745be738087359afca727863b839a7312a83
Taylor Phillips <taylorscottphillips@gmail.com>
2020-08-25 12:59:15 -0500

    Update Cell test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -30,10 +30,10 @@ class CellTest < MiniTest::Test
     cruiser = Ship.new("Cruiser", 3)
     cell = Cell.new("B4")
 
-    assert_equal false, cell.empty?
+    assert_equal true, cell.empty?
 
     cell.place_ship(cruiser)
-    assert_equal true, cell.empty?
+    assert_equal false, cell.empty?
   end
 
   def test_place_ship
@@ -66,5 +66,33 @@ class CellTest < MiniTest::Test
     cell.fire_upon
 
     assert_equal 2, cruiser.health
+  end
+
+  def test_render
+    # cell_1 = Cell.new("B4")
+
+    # assert_equal ".", cell_1.render
+
+    # cell_1.fire_upon
+    # assert_equal "M", cell_1.render
+
+    cell_2 = Cell.new("C3")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell_2.place_ship(cruiser)
+#require 'pry'; binding.pry
+    cell_2.render
+    assert_equal ".", cell_2.render
+    assert_equal "S", cell_2.render(true)
+
+    cell_2.fire_upon
+    assert_equal "H", cell_2.render
+
+    assert_equal false, cruiser.sunk?
+
+    cruiser.hit
+    cruiser.hit
+    assert_equal true, cruiser.sunk?
+    assert_equal "X", cell_2.render
   end
 end
