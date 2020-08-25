a8828aed9f84a792e6d0b5a92f15e81fec66286b
Sean Steel <seanrsteel@gmail.com>
2020-08-25 16:37:39 -0600

    Add cell hash and test board has attributes

---
+++ test/board_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+class BoardTest < MiniTest::Test
+
+  def test_it_exists
+    board = Board.new
+
+    assert_instance_of Board, board
+  end
+
+  def test_it_has_attributes
+    board = Board.new
+
+
+    assert_instance_of Hash, board.cells 
+  end
+
+end
