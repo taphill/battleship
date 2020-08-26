c3c8d66d7f4c3f5817342416328abfe113690241
Taylor Phillips <taylorscottphillips@gmail.com>
2020-08-26 09:58:59 -0500

    Refactor Board class

--- lib/board.rb
+++ lib/board.rb
@@ -5,27 +5,27 @@ class Board
 
   def initialize
     @cells = {
-      "A1" => Cell.new("A1"),
-      "A2" => Cell.new("A2"),
-      "A3" => Cell.new("A3"),
-      "A4" => Cell.new("A4"),
-      "B1" => Cell.new("B1"),
-      "B2" => Cell.new("B2"),
-      "B3" => Cell.new("B3"),
-      "B4" => Cell.new("B4"),
-      "C1" => Cell.new("C1"),
-      "C2" => Cell.new("C2"),
-      "C3" => Cell.new("C3"),
-      "C4" => Cell.new("C1"),
-      "D1" => Cell.new("D1"),
-      "D2" => Cell.new("D2"),
-      "D3" => Cell.new("D3"),
-      "D4" => Cell.new("D4"),
+      'A1' => Cell.new('A1'),
+      'A2' => Cell.new('A2'),
+      'A3' => Cell.new('A3'),
+      'A4' => Cell.new('A4'),
+      'B1' => Cell.new('B1'),
+      'B2' => Cell.new('B2'),
+      'B3' => Cell.new('B3'),
+      'B4' => Cell.new('B4'),
+      'C1' => Cell.new('C1'),
+      'C2' => Cell.new('C2'),
+      'C3' => Cell.new('C3'),
+      'C4' => Cell.new('C4'),
+      'D1' => Cell.new('D1'),
+      'D2' => Cell.new('D2'),
+      'D3' => Cell.new('D3'),
+      'D4' => Cell.new('D4')
     }
   end
 
   def valid_coordinate?(coordinate)
-    cells.has_key?(coordinate)
+    cells.key?(coordinate)
   end
 
   def valid_placement?(ship, coordinates)
@@ -46,9 +46,7 @@ class Board
 
   def place(ship, coordinates)
     coordinates.each do |coordinate|
-      if cells.has_key?(coordinate)
-        cells[coordinate].place_ship(ship)
-      end
+      cells[coordinate].place_ship(ship) if cells.key?(coordinate)
     end
   end
 
@@ -56,25 +54,26 @@ class Board
 
   def split_coordinates(coordinates)
     coordinates.map do |coordinate|
-      coordinate.split('').map do |character|
-        character.ord
-      end
+      coordinate.split('').map(&:ord)
+      # coordinate.split('').map do |character|
+      #   character.ord
+      # end
     end
   end
 
   def value_at_0_same?(coordinates)
     value = coordinates[0][0]
 
-    coordinates.all? do |block|
-      block[0] == value
+    coordinates.all? do |coordinate|
+      coordinate[0] == value
     end
   end
 
   def value_at_1_same?(coordinates)
     value = coordinates[0][1]
 
-    coordinates.all? do |block|
-      block[1] == value
+    coordinates.all? do |coordinate|
+      coordinate[1] == value
     end
   end
 
