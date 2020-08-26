8672597596b670bc5e543bf24ae00b41f9ecfbf5
Taylor Phillips <taylorscottphillips@gmail.com>
2020-08-26 18:30:26 -0500

    Add StartGame class and test

---
+++ lib/start_game.rb
@@ -0,0 +1,15 @@
+class StartGame
+  def initialize()
+    
+  end
+
+  def start
+    intro
+  end
+
+  private
+
+  def intro
+    puts "\nWelcome to BATTLESHIP\nEnter p to play. Enter q to quit."
+  end
+end
