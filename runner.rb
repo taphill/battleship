97c48745f90ac597e9bcb1b3e847e09b267492d9
Sean Steel <seanrsteel@gmail.com>
2020-09-02 08:06:56 -0600

    Refactor start_game class and update corrisponding files

--- runner.rb
+++ runner.rb
@@ -1,11 +1,13 @@
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
+require './lib/game'
 require './lib/start_game'
 
 cpu_board = Board.new
 player_board = Board.new
+game = Game.new(cpu_board, player_board)
 
-game = StartGame.new(cpu_board, player_board)
+run_game = StartGame.new(game)
 
-game.start
+run_game.start
--- test/start_game_test.rb
+++ test/start_game_test.rb
@@ -4,26 +4,17 @@ require './lib/ship'
 require './lib/cell'
 require './lib/board'
 require './lib/start_game'
+require './lib/game'
 
 class StartGameTest < MiniTest::Test
 
   def test_in_exists
     cpu_board = Board.new(6, 6)
     player_board = Board.new(6, 6)
+    game = Game.new(cpu_board, player_board)
 
-    game = StartGame.new(cpu_board, player_board)
+    start_game = StartGame.new(game)
 
-    assert_instance_of StartGame, game
-  end
-
-  def test_comp_ship_placement
-    #tested throught the start_game runner file.
-    #ensured valid placement and random placement.
-  end
-
-  def test_comp_ship_placement
-    #tested player input
-    #tested throught the start_game runner file.
-    #ensured valid placement and random placement.
+    assert_instance_of StartGame, start_game
   end
 end
