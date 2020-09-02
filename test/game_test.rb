11df9abae16f7931312d2e26612cb7eb9a63ec25
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-02 09:56:43 -0500

    Update tests for Game class

--- test/game_test.rb
+++ test/game_test.rb
@@ -3,11 +3,10 @@ require 'minitest/pride'
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
-require './lib/start_game'
 require './lib/game'
+require './lib/start_game'
 
 class GameTest < MiniTest::Test
-
   def test_it_exists
     cpu_board = Board.new(6, 6)
     player_board = Board.new(6, 6)
@@ -23,21 +22,25 @@ class GameTest < MiniTest::Test
 
     assert_equal cpu_board, game.cpu_board
     assert_equal player_board, game.player_board
+    assert_equal [], game.cpu_ships
+    assert_equal [], game.player_ships
   end
 
-  def test_reset
+  def test_it_can_reset
     cpu_board = Board.new(6, 6)
     player_board = Board.new(6, 6)
     game = Game.new(cpu_board, player_board)
 
     assert_equal 6, game.cpu_board.rows
     assert_equal 6, game.cpu_board.columns
+    assert_equal 6, game.player_board.rows
+    assert_equal 6, game.player_board.columns
 
     game.reset
 
     assert_equal 4, game.cpu_board.rows
     assert_equal 4, game.cpu_board.columns
+    assert_equal 4, game.player_board.rows
+    assert_equal 4, game.player_board.columns
   end
-
-
 end
